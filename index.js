c99fef6a25d5734a888ef6b00b02caefb3279e36
iqblfrdsyh <iqblfrdsyh@gmail.com>
2022-12-18 22:13:07 +0700

    new

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ import teamRoute from './routes/teamRoute.js'
 const app = express()
 const port = 3500
 
-app.use(cors())
 app.use(express.json())
 app.use(teamRoute)
+app.use(cors())
 
 const main = async () => {
     await mongoose.connect('mongodb+srv://iqblfrdsyh:punyaiqbal@cluster0.t6cc0tu.mongodb.net/MilikKita?retryWrites=true&w=majority').finally(console.log(`Connected`))
 }
 
 main().catch((err) => console.log(err))
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
